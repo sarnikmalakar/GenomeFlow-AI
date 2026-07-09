3fbea2432148c67ff632a11f1b31fc4e96c952ee
Sarnik Malakar <sarnikmalakar@gmail.com>
2026-07-09 18:18:50 +0530

    minor changes

--- ml/train.py
+++ ml/train.py
@@ -69,7 +69,7 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(
         X,
         y,
-        test_size=0.20,
+        test_size=0.30,
         random_state=42,
         stratify=y,
     )
